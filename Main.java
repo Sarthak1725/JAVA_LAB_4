47cfe6f9ddc6a0e19bb56f1dbe0c5cde40823fc4
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-06 00:00:06 +0530

    Menu driven program created

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@
 //23070126088
 //AIML B1
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
 
@@ -9,6 +11,20 @@ public class Main{
         Deck deck = new Deck();
         int choice;
         do { 
+
+             System.out.println("\nMENU");
+            System.out.println("1. Print Deck");
+            System.out.println("2. Print a Specific Card");
+            System.out.println("3. Find Cards of Same Suit");
+            System.out.println("4. Find Cards of Same Rank");
+            System.out.println("5. Find a Specific Card");
+            System.out.println("6. Deal 5 Random Cards");
+            System.out.println("7. Shuffle Deck");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine();
             
         } while (true);
 
