5e60c33ebb0c72713de9d1d2121b97c6e13e3cb1
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-06 00:13:10 +0530

    Deck created

--- Deck.java
+++ Deck.java
@@ -8,5 +8,16 @@ class Deck {
         createDeck();
     }
 
+    public void createDeck() {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+
+        for (String rank : ranks) {
+            for (String suit : suits) {
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
+
     
 }
