a6954b37d6fd7adbf9f87c78695516a649947b7c
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-05 23:57:46 +0530

    inout addded

--- Main.java
+++ Main.java
@@ -3,5 +3,12 @@
 //AIML B1
 
 public class Main{
-    
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
+        int choice;
+
+    }
+
 }
