20905d88790ba410d02d467ed77ea091137adfef
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-06 00:12:16 +0530

    Deck added

--- Deck.java
+++ Deck.java
@@ -1,4 +1,12 @@
 import java.util.*;
 class Deck {
+    ArrayList<Card> deck;
+
+    // Constructor initializes the deck and creates all 52 cards
+    public Deck() {
+        deck = new ArrayList<>();
+        createDeck();
+    }
+
     
 }
